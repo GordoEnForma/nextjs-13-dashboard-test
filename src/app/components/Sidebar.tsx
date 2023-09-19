f0fce8d5ac993529a60a51b7a884bf8bc1b0d391
Gustavo <alonsoem9@gmail.com>
2023-09-19 01:01:56 -0500

    Minor Style Change: Removed Overflow-y

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -22,7 +22,7 @@ export const Sidebar = () => {
     <div
       id="menu"
       style={{ width: "400px" }}
-      className="bg-gray-900 min-h-screen z-10 text-slate-300 w-64  left-0 h-screen overflow-y-scroll"
+      className="bg-gray-900 min-h-screen z-10 text-slate-300 w-64  left-0 h-screen"
     >
       <div id="logo" className=" my-4 px-6">
         <h1 className="flex items-center text-lg md:text-2xl font-bold text-white">
---
+++ src/app/dashboard/pokemons/page.tsx
@@ -0,0 +1,8 @@
+
+export default function PokemonsPage() {
+  return (
+    <div>
+      <h1>Hello Pokemons</h1>
+    </div>
+  );
+}
