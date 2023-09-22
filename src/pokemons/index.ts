2499ddae1d706f1a62a217d6c54874d3b0dd6f33
Gustavo <alonsoem9@gmail.com>
2023-09-21 23:55:49 -0500

    Dynamic metadata and notfound page

--- src/pokemons/index.ts
+++ src/pokemons/index.ts
@@ -1,4 +1,5 @@
 export type {PokemonResponse} from './interfaces/pokemons-response';
 export type {SimplePokemon} from './interfaces/simple-pokemon';
+export type {Pokemon} from './interfaces/pokemon';
 export {PokemonGrid} from './components/PokemonGrid';
 export {PokemonCard} from './components/PokemonCard';
