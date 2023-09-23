9c65a9165ff7aae85767160c5974ea3c0d898e52
Gustavo <alonsoem9@gmail.com>
2023-09-22 22:16:54 -0500

    Not found page for pokemons

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -9,7 +9,7 @@ export default function DashboardLayout({
       <div className="bg-slate-100 w-screen overflow-y-scroll h-screen antialiased text-slate-300 selection:bg-blue-600 selection:text-white">
         <div className="flex">
           <Sidebar />
-          <div className="p-2 w-full text-slate-900">
+          <div className="w-full text-slate-900">
             {children}
           </div>
         </div>
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -13,17 +13,17 @@ export default function NotFound() {
           <div className="bg-[#FF6A3D] px-2 text-sm rounded rotate-12 absolute">
             Page Not Found
           </div>
-          <button className="mt-5">
-            <div className="relative inline-block text-sm font-medium text-[#FF6A3D] group active:text-orange-500 focus:outline-none focus:ring">
-              <span className="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-[#FF6A3D] group-hover:translate-y-0 group-hover:translate-x-0"></span>
+          <Link href="/dashboard/main">
+            <button className="mt-5">
+              <div className="relative inline-block text-sm font-medium text-[#FF6A3D] group active:text-orange-500 focus:outline-none focus:ring">
+                <span className="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-[#FF6A3D] group-hover:translate-y-0 group-hover:translate-x-0"></span>
 
-              <span className="relative block px-8 py-3 bg-[#1A2238] border border-current">
-                <Link href="/dashboard/main">
+                <span className="relative block px-8 py-3 bg-[#1A2238] border border-current">
                   Regresar a la página principal
-                </Link>
-              </span>
-            </div>
-          </button>
+                </span>
+              </div>
+            </button>
+          </Link>
         </main>
       </div>
     </div>
